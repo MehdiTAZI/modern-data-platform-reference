2bafd0d1f863e4f645fc990be9ff7bdd1e1f7b2e
Mehdi TAZI <2340163+MehdiTAZI@users.noreply.github.com>
2026-08-29 16:39:40 +0100

    fix: pin current AzureRM provider line

--- infra/stacks/state-backend/versions.tf
+++ infra/stacks/state-backend/versions.tf
@@ -1,4 +1,15 @@
-terraform { required_version = "~> 1.15.0"
-  required_providers { azurerm = { source = "hashicorp/azurerm" version = "~> 4.0" } }
+terraform {
+  required_version = "~> 1.15.0"
+
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "~> 4.80"
+    }
+  }
 }
-provider "azurerm" { features {} subscription_id = var.subscription_id }
+
+provider "azurerm" {
+  features {}
+  subscription_id = var.subscription_id
+}
