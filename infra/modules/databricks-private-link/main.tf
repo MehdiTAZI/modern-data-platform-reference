63cd31de56175f537d225c7066f972ddedf47065
Mehdi TAZI <2340163+MehdiTAZI@users.noreply.github.com>
2026-08-31 00:56:33 +0100

    fix(terraform): migrate Private DNS link to AzureRM v5 schema

--- infra/modules/databricks-private-link/main.tf
+++ infra/modules/databricks-private-link/main.tf
@@ -6,8 +6,7 @@ resource "azurerm_private_dns_zone" "databricks" {
 
 resource "azurerm_private_dns_zone_virtual_network_link" "databricks" {
   name                  = "${var.name_prefix}-databricks"
-  resource_group_name   = var.resource_group_name
-  private_dns_zone_name = azurerm_private_dns_zone.databricks.name
+  private_dns_zone_id   = azurerm_private_dns_zone.databricks.id
   virtual_network_id    = var.vnet_id
   registration_enabled  = false
   tags                  = var.tags
