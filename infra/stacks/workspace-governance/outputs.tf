a9727cfcf698b6706c984385d56b81d7a8aeab8b
Mehdi TAZI <2340163+MehdiTAZI@users.noreply.github.com>
2026-08-30 14:35:15 +0100

    feat(terraform): expose serverless NCC outputs

--- infra/stacks/workspace-governance/outputs.tf
+++ infra/stacks/workspace-governance/outputs.tf
@@ -5,3 +5,11 @@ output "catalog_name" {
 output "landing_volume" {
   value = module.uc.landing_volume
 }
+
+output "serverless_ncc_id" {
+  value = try(module.serverless_ncc[0].network_connectivity_config_id, null)
+}
+
+output "serverless_private_endpoint_rule_ids" {
+  value = try(module.serverless_ncc[0].private_endpoint_rule_ids, {})
+}
